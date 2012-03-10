2869c2b29c67e281c04d7c0368ce7797f23fcdaa
Alysson Souza <alysson87@gmail.com>
2012-03-10 16:50:47 -0300

    added url to message changed tabs to spaces cosmetics added author info

--- get-youtube-video-info.py
+++ get-youtube-video-info.py
@@ -4,49 +4,57 @@ import xchat
 import re
 
 __module_name__        = "Get Youtube Video Info"
-__module_version__     = "0.1"
+__module_version__     = "0.2"
 __module_description__ = "Reads and displays video info from an URL."
+__module_author__      = "demi_alucard <alysson87@gmail.com>"
 
-def get_yt_info(id):  
-    params = {}
-    params['v'] = '2.1'
-    params['alt'] = 'json'
-    params['safeSearch'] = 'none'
+def get_yt_info(id):
+    params                = {}
+    params['v']           = '2.1'
+    params['alt']         = 'json'
+    params['safeSearch']  = 'none'
     params['restriction'] = '255.255.255.255'
-    params['q'] = '"%s"' % id
-    params['fields'] = 'entry[media:group/yt:videoid="%s"]' % id
-    params = urllib.urlencode(params)
-    
+    params['q']           = '"%s"' % id
+    params['fields']      = 'entry[media:group/yt:videoid="%s"]' % id
+    params                = urllib.urlencode(params)
+
     f = urllib.urlopen('http://gdata.youtube.com/feeds/api/videos?%s' % params)
     data = json.load(f)
-    
+
     info = {}
-    info['title'] = data['feed']['entry'][0]['title']['$t']
-    info['views'] = data['feed']['entry'][0]['yt$statistics']['viewCount']
+    info['id']        = id
+    info['title']     = data['feed']['entry'][0]['title']['$t']
+    info['views']     = data['feed']['entry'][0]['yt$statistics']['viewCount']
     info['favorites'] = data['feed']['entry'][0]['yt$statistics']['favoriteCount']
-    info['likes'] = data['feed']['entry'][0]['yt$rating']['numLikes']
-    info['dislikes'] = data['feed']['entry'][0]['yt$rating']['numDislikes']
-    
+    info['likes']     = data['feed']['entry'][0]['yt$rating']['numLikes']
+    info['dislikes']  = data['feed']['entry'][0]['yt$rating']['numDislikes']
+
     return info
 
 def show_yt_info(info):
-    print "3:: YouTube: %s 3:: Views: %s 3:: [+] %s likes 4[-] %s dislikes 3::" % \
-          (info['title'], info['views'], info['likes'], info['dislikes'])
+    print \
+    ("\0033\002::\003 YouTube\002 %s " + \
+    "\0033\002::\003 URL:\002 http://youtu.be/%s " + \
+    "\0033\002::\003 Views:\002 %s " + \
+    "\0033\002:: [+]\002 %s likes " + \
+    "\002\0034[-]\002 %s dislikes " + \
+    "\0033\002::\002") % \
+          (info['title'], info['id'], info['views'], info['likes'], info['dislikes'])
 
 def get_id_from_url(text):
     check = re.compile(r"(?:https?\://)?(?:\w+\.)?(?:youtube|youtu)(?:\.\w+){1,2}/")
     normal = re.compile(r"(?:=|%3F)?v(?:=|%3D)([\w\-]+)")
-    fallback = re.compile(r"(?:(?:https?\://)?(?:\w+\.)?(?:youtube|youtu)(?:\.\w+){1,2}/)([\w\-]+)")
+    short = re.compile(r"(?:(?:https?\://)?(?:\w+\.)?(?:youtube|youtu)(?:\.\w+){1,2}/)([\w\-]+)")
     vid = ""
-    
+
     if re.search(check, text):
         normal = normal.search(text)
-        fallback = fallback.search(text)
+        short = short.search(text)
         if normal != None:
             vid = normal.group(1)
-        elif fallback != None:
-            vid = fallback.group(1)
-            
+        elif short != None:
+            vid = short.group(1)
+
     return vid
 
 def privmsg_cb(word, word_eol, userdata):
@@ -59,7 +67,7 @@ def privmsg_cb(word, word_eol, userdata):
     show_yt_info(info)
     return xchat.EAT_NONE
 
-def yt_cb(word, word_eol, userdata):
+def ytcmd_cb(word, word_eol, userdata):
     if len(word) < 2:
         return xchat.EAT_NONE
     id = get_id_from_url(word_eol[0])
@@ -71,6 +79,6 @@ def yt_cb(word, word_eol, userdata):
     show_yt_info(info)
     return xchat.EAT_NONE
 
-xchat.hook_command("yt", yt_cb, help="/yt <url> to get video info")
+xchat.hook_command("yt", ytcmd_cb, help="/yt <url> to get video info")
 xchat.hook_server("PRIVMSG", privmsg_cb)
 
